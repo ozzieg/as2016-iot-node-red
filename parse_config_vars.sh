ab570efba8f683dc3bd814c60ebcd337ef4ba1bd
Ozzie Gurkan <ogurkan@salesforce.com>
2016-03-21 13:31:04 -0700

    now parsing postgres and access token on startup

--- parse_config_vars.sh
+++ parse_config_vars.sh
@@ -21,5 +21,17 @@ part1=${DATABASE_URL#postgres:*:*:}
 POSTGRESDB_DATABASE=${part1#*/}
 echo $POSTGRESDB_DATABASE
 
+part1=${DATABASE_URL#postgres://*}
+part2=${part1%@*}
+POSTGRESDB_USERNAME=${part2%:*}
+POSTGRESDB_PASSWORD=${part2#*:}
+echo $POSTGRESDB_USERNAME $POSTGRESDB_PASSWORD
+
+#modify flow_creds.json and write the postgresdb username and password
+sed -i -e 's/postgresdb_user/'$POSTGRESDB_USERNAME'/g' defaults/flow_creds.json
+sed -i -e 's/postgresdb_password/'$POSTGRESDB_PASSWORD'/g' defaults/flow_creds.json
+
+#modify flow_creds.json and write particle access token
+sed -i -e 's/PARTICLE_ACCESS_TOKEN/'$PARTICLE_ACCESS_TOKEN'/g' defaults/flow_creds.json
 
 export MONGODB_PORT MONGODB_DATABASE MONGODB_URL POSTGRESDB_URL POSTGRESDB_DATABASE
