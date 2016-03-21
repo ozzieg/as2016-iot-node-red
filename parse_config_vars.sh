38092f82f0abac655652b63bdbab3a8b8797190f
Ozzie Gurkan <ogurkan@salesforce.com>
2016-03-21 15:42:47 -0700

    using $HOME variable

--- parse_config_vars.sh
+++ parse_config_vars.sh
@@ -3,8 +3,7 @@
 MONGODB_URL=$MONGOLAB_URI
 POSTGRESDB_URL=$DATABASE_URL
 
-echo $pwd
-
+echo "Setting Environment Variables"
 part1=${MONGODB_URL#mongodb://}
 MONGODB_URL=${part1%:*}
 echo $MONGODB_URL
@@ -30,10 +29,10 @@ POSTGRESDB_PASSWORD=${part2#*:}
 echo $POSTGRESDB_USERNAME $POSTGRESDB_PASSWORD
 
 #modify flow_creds.json and write the postgresdb username and password
-sed -i -e "s/postgresdb_user/$POSTGRESDB_USERNAME/g" "/app/defaults/flow_creds.json"
-sed -i -e "s/postgresdb_password/$POSTGRESDB_PASSWORD/g" "/app/defaults/flow_creds.json"
+sed -i -e "s/postgresdb_user/$POSTGRESDB_USERNAME/g" "$HOME/defaults/flow_creds.json"
+sed -i -e "s/postgresdb_password/$POSTGRESDB_PASSWORD/g" "$HOME/defaults/flow_creds.json"
 
 #modify flow_creds.json and write particle access token
-sed -i -e "s/PARTICLE_ACCESS_TOKEN/$PARTICLE_ACCESS_TOKEN/g" "/app/defaults/flow_creds.json"
+sed -i -e "s/PARTICLE_ACCESS_TOKEN/$PARTICLE_ACCESS_TOKEN/g" "$HOME/defaults/flow_creds.json"
 
 export MONGODB_PORT MONGODB_DATABASE MONGODB_URL POSTGRESDB_URL POSTGRESDB_DATABASE
